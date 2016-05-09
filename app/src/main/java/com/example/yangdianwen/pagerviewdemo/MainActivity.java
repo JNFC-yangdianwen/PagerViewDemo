b7833e54d534edb0fe5eb5b2736d5af713c451b0
JNFC-yangdianwen <2550286303@qq.com>
2016-05-09 14:52:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yangdianwen/pagerviewdemo/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.yangdianwen.pagerviewdemo;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ViewPager mViewPager;
+    private ArrayList<View> mlist;
+    int[] pic = {R.drawable.adware_style_applist,
+            R.drawable.adware_style_banner,
+            R.drawable.adware_style_creditswall};
+    private TextView tv_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        mlist = new ArrayList<>();
+        mViewPager = (ViewPager) findViewById(R.id.vp_guide);
+        for (int i = 0; i < pic.length; i++) {
+            ImageView iv = new ImageView(this);
+            iv.setImageResource(pic[i]);
+            mlist.add(iv);
+        }
+        mViewPager.setAdapter(new myAdapter(mlist));
+        //设置翻页动画
+        mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        tv_text = (TextView) findViewById(R.id.tv_show);
+        tv_text.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, Second.class);
+        startActivity(intent);
+
+    }
+
+    private class myAdapter extends PagerAdapter {
+        private ArrayList<View> mList;
+
+        public myAdapter(ArrayList<View> list) {
+            this.mList = list;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mList.get(position), 0);
+            return mList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mList.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            if (mList != null) {
+                return mList.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+    }
+}
+
+class DepthPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.75f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0);
+
+        } else if (position <= 0) { // [-1,0]
+            // Use the default slide transition when moving to the left page
+            view.setAlpha(1);
+            view.setTranslationX(0);
+            view.setScaleX(1);
+            view.setScaleY(1);
+
+        } else if (position <= 1) { // (0,1]
+            // Fade the page out.
+            view.setAlpha(1 - position);
+
+            // Counteract the default slide transition
+            view.setTranslationX(pageWidth * -position);
+
+            // Scale the page down (between MIN_SCALE and 1)
+            float scaleFactor = MIN_SCALE
+                    + (1 - MIN_SCALE) * (1 - Math.abs(position));
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0);
+        }
+    }
+}
+
+class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.85f;
+    private static final float MIN_ALPHA = 0.5f;
+
+    @SuppressLint("NewApi")
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+        int pageHeight = view.getHeight();
+
+        Log.e("TAG", view + " , " + position + "");
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0);
+
+        } else if (position <= 1) //a页滑动至b页 ； a页从 0.0 -1 ；b页从1 ~ 0.0
+        { // [-1,1]
+            // Modify the default slide transition to shrink the page as well
+            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+            if (position < 0) {
+                view.setTranslationX(horzMargin - vertMargin / 2);
+            } else {
+                view.setTranslationX(-horzMargin + vertMargin / 2);
+            }
+
+            // Scale the page down (between MIN_SCALE and 1)
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+            // Fade the page relative to its size.
+            view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE)
+                    / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0);
+        }
+    }
+}
